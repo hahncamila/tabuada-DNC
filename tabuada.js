250f5d1609d34c7eeb8b61b203c63bcc8da2ad9b
Camila Hahn <106129904+hahncamila@users.noreply.github.com>
2024-02-06 11:29:13 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,20 @@
+function Tabuada(){
+    var num = window.document.getElementById('num')
+    var end = document.getElementById('end')
+    if (num.value.length == 0 || end.value.length == 0){
+        window.alert('É necessário preencher todos os campos!')
+    } else {
+        var n = Number(num.value)
+        var e = Number(end.value)
+        var x = 0
+        var r = ''
+        var res = window.document.getElementById('resposta')
+        res.innerHTML = `A tabuada do ${n} é: <br>`
+            
+        do{
+            r = n*x
+            res.innerHTML += `${x} X ${n} = ${r} <br>`
+            x++
+        } while(x <= e) 
+    }
+}
